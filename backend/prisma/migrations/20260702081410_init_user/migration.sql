70afa35d459c6a2e02b0b8c235b2cde5e8b3e32d
Anshika Singh <anshisingh1231@gmail.com>
2026-07-02 13:49:13 +0530

    init db

---
+++ backend/prisma/migrations/20260702081410_init_user/migration.sql
@@ -0,0 +1,19 @@
+-- CreateEnum
+CREATE TYPE "AuthProvider" AS ENUM ('EMAIL', 'GOOGLE');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "passwordHash" TEXT,
+    "provider" "AuthProvider" NOT NULL DEFAULT 'EMAIL',
+    "profilePicture" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
